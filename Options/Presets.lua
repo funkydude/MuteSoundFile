ee4c7b432f06c6759c450ea62f4ac100e7f0259e
Adam Koch <me@adamko.ch>
2022-03-12 20:33:06 -0500

    Add Mount: Sunwarmed Furline Preset (#21)

--- Options/Presets.lua
+++ Options/Presets.lua
@@ -10,4 +10,17 @@ scope.presets = {
 		640180, 640182, 640184, -- vo_grummle_kooky_vendor_01 / 2 / 3
 		640158, 640160, 640162, 640164, -- vo_grummle_kooky_farewell_01 / 2 / 3 / 4
 	},
+	sunwarmedFurline = {
+		3598605, -- catmount_aggro_3598605
+		3598607, -- catmount_oneshot_3598607
+		3598611, -- catmount_oneshot_3598611
+		3598613, -- catmount_oneshot_3598613
+		3598617, -- catmount_oneshot_3598617
+		3598619, -- catmount_oneshot_3598619
+		3598621, -- catmount_oneshot_3598621
+		3598623, -- catmount_oneshot_3598623
+		3598625, -- catmount_oneshot_3598625
+		3598635, -- catmount_cast_oneshot_3598635
+		3598637, -- catmount_cast_oneshot_3598637
+	},
 }
