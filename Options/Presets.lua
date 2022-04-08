b1ce2b619f029cf70240d7bd18242c75aa083701
numbersix <brunochampoux@users.noreply.github.com>
2022-04-08 14:19:25 -0400

    Added presets for train and moo emotes from all races and genders (#23)

--- Options/Presets.lua
+++ Options/Presets.lua
@@ -23,4 +23,63 @@ scope.presets = {
 		3598635, -- catmount_cast_oneshot_3598635
 		3598637, -- catmount_cast_oneshot_3598637
 	},
+	choochooEmotes = {
+		540734, -- humanmalechoochoo01
+		540535, -- humanfemalechoochoo01
+		539881, -- dwarfmalechoochoo01
+		539802, -- dwarffemalechoochoo01
+		540271, -- gnomefemalechoochoo01
+		540275, -- gnomemalechoochoo01
+		540870, -- nightelffemalechoochoo01
+		540947, -- nightelfmalechoochoo01
+		541239, -- orcmalechoochoo01
+		541157, -- orcfemalechoochoo01
+		542600, -- undeadmalechoochoo01
+		542526, -- undeadfemalechoochoo01
+		542896, -- taurenmalechoochoo01
+		542818, -- taurenfemalechoochoo01
+		543093, -- trollmalechoochoo01
+		543085, -- trollfemalechoochoo01
+		539219, -- bloodelffemaletrain01
+		539203, -- bloodelfmaletrain01
+		539516, -- draeneifemaletrain01
+		539730, -- draeneimaletrain01
+		541463, -- vo_pcgilneanfemale_train01
+		541601, -- vo_pcgilneanmale_train01
+		542017, -- vo_pcgoblinmale_train01
+		541769, -- vo_pcgoblinfemale_train01
+		542206, -- vo_pcworgenmale_train01
+		542035, -- vo_pcworgenfemale_train01
+		630296, -- vo_pcpandarenmale_train01
+		630298, -- vo_pcpandarenmale_train02
+		636621, -- vo_pcpandarenfemale_train01
+		1306531, -- vo_dhbloodelfmaletrain01
+		1313588, -- vo_dhbloodelffemale_train01
+		1304872, -- vo_dhnightelfmalechoochoo01
+		1316209, -- vo_dhnightelffemale_choochoo01
+		1733163, -- vo_735_pc_-_void_elf_male_194_m
+		1732785, -- vo_735_pc_-_void_elf_female_194_f
+		1730908, -- vo_735_pc_-_highmountain_tauren_male_193_m
+		1730534, -- vo_735_pc_-_highmountain_tauren_female_193_f
+		1731282, -- vo_735_pc_-_lightforged_draenei_female_193_f
+		1731656, -- vo_735_pc_-_lightforged_draenei_male_193_m
+		1732030, -- vo_735_pc_-_nightborne_elf_female_193_f
+		1732405, -- vo_735_pc_-_nightborne_elf_male_193_m
+		1902030, -- vo_801_pc_dark_iron_dwarf_female_342_f
+		1902543, -- vo_801_pc_dark_iron_dwarf_male_342_m
+		1951457, -- vo_801_pc_maghar_orc_female_157_f
+		1951458, -- vo_801_pc_maghar_orc_male_157_m
+		1903049, -- vo_801_pc_zandalari_troll_female_335_f
+		1903522, -- vo_801_pc_zandalari_troll_male_295_m
+		2531204, -- vo_815_pc_kul_tiran_human_female_196_f
+		2491898, -- vo_815_pc_kul_tiran_human_male_16_m
+		3107182, -- vo_83_pc_mechagnome_male_155_m
+		3107651, -- vo_83_pc_mechagnome_female_155_f
+		3106252, -- vo_83_pc_vulpera_female_155_f
+		3106717, -- vo_83_pc_vulpera_male_155_m
+	},
+	mooEmotes = {
+		542820, -- taurenfemalemoo01
+		542894, -- taurenmalemoo01
+	},
 }
